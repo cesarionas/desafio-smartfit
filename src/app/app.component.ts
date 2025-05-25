53868ae9991bee26b38e14af363a116d4412682b
Vinicius Cesario <vini.cesario@gmail.com>
2025-05-25 14:43:29 -0300

    create form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-import { NgOptimizedImage } from '@angular/common';
+import { FormsComponent } from './components/forms/forms.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FormsComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
